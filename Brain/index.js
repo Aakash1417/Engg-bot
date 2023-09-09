a8aa643cb921d364b3d8278dbd011f8f0b93d84e
Aakash1417 <info4aakash@gmail.com>
2023-09-09 12:26:18 -0600

    front and backend working

---
+++ Brain/index.js
@@ -0,0 +1,39 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+  organization: "org-",
+  apiKey: "sk-",
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are Engg-Bot! Your role is to assist users with engineering questions and problems. Provide detailed explanations, step-by-step instructions, and relevant insights. If a user asks for clarification or additional information, be patient and ask probing questions to better understand their needs. Always strive to help users solve their engineering challenges effectively and efficiently.",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
